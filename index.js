01b9f61c24e5e28c844e996ef157b3f3798bd131
Shariar <shariarnafis86@gmail.com>
2023-10-26 20:06:29 +0600

    get service data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,6 +26,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const serverCollection = client.db('carDoctor').collection('services');
+    const orderCollection = client.db('carDoctor').collection('orderedServices');
+
+
+
+    app.get('/services', async (req, res) => {
+      const cursor = serverCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = {
+        projection: { title: 1, service_id: 1, price: 1, img:1 }
+      }
+      const result = await serverCollection.findOne(query, options)
+      res.send(result)
+    })
+
+    app.post('/orderedService', async(req, res)=>{
+      const ordered = req.body;
+      console.log(ordered)
+    })
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -38,10 +65,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send('car doctor server is running')
+app.get('/', (req, res) => {
+  res.send('car doctor server is running')
 })
 
-app.listen(port, ()=>{
-    console.log('car doctor server is running')
+app.listen(port, () => {
+  console.log('car doctor server is running')
 })
